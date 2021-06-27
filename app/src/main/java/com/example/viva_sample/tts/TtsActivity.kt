bfd35337f7918867969675c06bf4e675c9725931
Matthew <ansxodud5312@gmail.com>
2021-06-28 01:54:25 +0900

    # BLE 테스트

---
+++ app/src/main/java/com/example/viva_sample/tts/TtsActivity.kt
@@ -0,0 +1,11 @@
+package com.example.viva_sample.TTS
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TtsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tts)
+    }
+}
